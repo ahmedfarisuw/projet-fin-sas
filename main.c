aa95f817a3cc24356785535641430f63adcc7a94
ahmed faris <ahmedfarisuw05@gmail.com>
2025-09-10 14:10:41 +0100

    projet-fin-sas Affichage

---
+++ main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_JOUEURS 100
+
+typedef struct {
+    int id;
+    char nom[50];
+    char prenom[50];
+    int numeroMaillot;
+    char poste[50];
+    int age;
+    int buts;
+    char dateInscription[50];
+    char statut[50];
+} joueur;
+
+joueur equipe[MAX_JOUEURS];
+int nombreJoueurs = 0;
+int ajoutID = 1;
+void ajouterJoueur() {
+    if (nombreJoueurs >= MAX_JOUEURS) {
+        printf("⚠ Impossible d'ajouter plus de joueurs !\n");
+        return;
+    }
+
+    joueur j;
+    j.id = ajoutID++;
+
+    printf("\n--- Ajouter un joueur ---\n");
+    printf("Nom : ");
+    scanf("%s", j.nom);
+    printf("Prenom : ");
+    scanf("%s", j.prenom);
+    printf("Numero de maillot : ");
+    scanf("%d", &j.numeroMaillot);
+    printf("Poste (gardien/defenseur/milieu/attaquant) : ");
+    scanf("%s", j.poste);
+    printf("Age : ");
+    scanf("%d", &j.age);
+    printf("Nombre de buts : ");
+    scanf("%d", &j.buts);
+    printf("Statut (titulaire/remplacant) : ");
+    scanf("%s", j.statut);
+
+    equipe[nombreJoueurs++] = j;
+    printf("✅ Joueur ajoute avec succes ! ID = %d\n", j.id);
+}
+void ajouterPlusieursJoueurs() {
+    int n;
+    printf("\nCombien de joueurs voulez-vous ajouter ? ");
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        ajouterJoueur();
+    }
+}
+int main() {
+    int choix;
+    do {
+        printf("\n===== MENU =====\n");
+        printf("1. Ajouter un joueur\n");
+        printf("2. Ajouter plusieurs joueurs\n");
+        printf("0. Quitter\n");
+        printf("Votre choix : ");
+        scanf("%d", &choix);
+
+        switch (choix) {
+            case 1: ajouterJoueur(); break;
+            case 2: ajouterPlusieursJoueurs(); break;
+            case 0: printf("👋 Au revoir !\n"); break;
+            default: printf("⚠ Choix invalide.\n");
+        }
+    } while (choix != 0);
+
+    return 0;
+}
+void afficherjoueurs(){
+    if(nombreJoueurs == 0){
+        printf("aucun joueur dans équipe \n");
+        return;
+    }
+    printf("\n--- Liste des joueurs ---\n");
+    for (int i = 0; i < nombreJoueurs; i++) {
+        Joueur j = equipe[i];
+        printf("ID:%d | %s %s | Maillot:%d | Poste:%s | Age:%d | Buts:%d | Date:%s | Statut:%s\n",
+               j.id, j.nom, j.prenom, j.numeroMaillot, j.poste, j.age, j.buts, j.dateInscription, j.statut);
+    }
+}
